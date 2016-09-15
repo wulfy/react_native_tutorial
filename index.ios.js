24da41924067766b543252018df0907548247f78
Ludovic Lasry <llasry@ludo-mac.home>
2016-09-15 23:52:19 +0200

    adding components example with props

--- index.ios.js
+++ index.ios.js
@@ -9,16 +9,32 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
 
+
+class Bananas extends Component {
+  render() {
+    
+    return (
+      <Image source={this.props.url} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
 class react_native_tutorial extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native IOS!
         </Text>
+        <Bananas url={pic} />
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
